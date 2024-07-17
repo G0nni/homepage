739ebd1f6b2fe623ae60b4902136a3f4021469ad
Gonni <nicolas.cros.sn@gmail.com>
2024-07-17 22:11:11 +0200

    sync test

--- src/app/_components/syncModule.tsx
+++ src/app/_components/syncModule.tsx
@@ -10,15 +10,16 @@ type Session = {
   [key: string]: unknown;
 };
 
-type userConfig = {
-  autoThemeEnabled: boolean;
-  tabs: string;
-  themeImage: string;
-  searchEngine: string;
-  topColor: string;
-  bottomColor: string;
-  darkVibrant: string;
-  lightVibrant: string;
+type UserConfig = {
+  autoThemeEnabled: boolean | null;
+  tabs: string | null;
+  themeImage: string | null;
+  searchEngine: string | null;
+  topColor: string | null;
+  bottomColor: string | null;
+  darkVibrant: string | null;
+  lightVibrant: string | null;
+  userId: string | null; // Add the 'userId' property
 };
 
 interface SyncModuleProps {
@@ -26,23 +27,38 @@ interface SyncModuleProps {
 }
 
 export function SyncModule({ session }: SyncModuleProps) {
-  const [userConfig, setUserConfig] = useState<userConfig | null>(null);
+  const [userConfig, setUserConfig] = useState<UserConfig | null>(null);
+  const [serverUserConfig, setServerUserConfig] = useState<UserConfig | null>(
+    null,
+  );
 
   const createConfig = api.userConfig.create.useMutation({
-    onSuccess: async () => {
+    onSuccess: () => {
       console.log("creation success");
     },
   });
+
   const updateConfig = api.userConfig.update.useMutation({
-    onSuccess: async () => {
+    onSuccess: () => {
       console.log("update success");
     },
   });
 
+  const fetchServerConfig = api.userConfig.getByUserId.useQuery(
+    { userId: session?.user.id || "" },
+    { enabled: !!session?.user.id },
+  );
+
   useEffect(() => {
-    const SyncStorageCheck = () => {
+    if (fetchServerConfig.data) {
+      setServerUserConfig(fetchServerConfig.data);
+    }
+  }, [fetchServerConfig.data]);
+
+  useEffect(() => {
+    const syncStorageCheck = () => {
       if (session) {
-        console.log("SyncStorageCheck");
+        console.log("syncStorageCheck");
         const autoThemeEnabled = localStorage.getItem("autoThemeEnabled");
         const tabs = localStorage.getItem("tabs");
         const themeImage = localStorage.getItem("themeImage");
@@ -50,33 +66,31 @@ export function SyncModule({ session }: SyncModuleProps) {
         const topColor = localStorage.getItem("topColor");
         const bottomColor = localStorage.getItem("bottomColor");
         const darkVibrant = localStorage.getItem("darkVibrant");
-        console.log("darkVibrant", darkVibrant);
         const lightVibrant = localStorage.getItem("lightVibrant");
-        console.log("lightVibrant", lightVibrant);
 
         setUserConfig({
+          userId: session.user.id,
           autoThemeEnabled: autoThemeEnabled
             ? JSON.parse(autoThemeEnabled)
             : false,
-          tabs: tabs ? tabs : "",
-          themeImage: themeImage ? themeImage : "",
-          searchEngine: searchEngine ? searchEngine : "",
-          topColor: topColor ? topColor : "",
-          bottomColor: bottomColor ? bottomColor : "",
-          darkVibrant: darkVibrant ? darkVibrant : "",
-          lightVibrant: lightVibrant ? lightVibrant : "",
+          tabs: tabs || "",
+          themeImage: themeImage || "",
+          searchEngine: searchEngine || "",
+          topColor: topColor || "",
+          bottomColor: bottomColor || "",
+          darkVibrant: darkVibrant || "",
+          lightVibrant: lightVibrant || "",
         });
       } else {
-        console.log("SyncStorageCheck: No session");
+        console.log("syncStorageCheck: No session");
       }
     };
 
-    SyncStorageCheck();
-
-    window.addEventListener("storage", SyncStorageCheck);
+    syncStorageCheck();
+    window.addEventListener("storage", syncStorageCheck);
 
     return () => {
-      window.removeEventListener("storage", SyncStorageCheck);
+      window.removeEventListener("storage", syncStorageCheck);
     };
   }, [session]);
 
@@ -88,6 +102,43 @@ export function SyncModule({ session }: SyncModuleProps) {
 
   const handleSync = () => {
     console.log(userConfig);
+    if (session && userConfig) {
+      if (serverUserConfig) {
+        if (
+          userConfig.autoThemeEnabled !== serverUserConfig.autoThemeEnabled ||
+          userConfig.tabs !== serverUserConfig.tabs ||
+          userConfig.themeImage !== serverUserConfig.themeImage ||
+          userConfig.searchEngine !== serverUserConfig.searchEngine ||
+          userConfig.topColor !== serverUserConfig.topColor ||
+          userConfig.bottomColor !== serverUserConfig.bottomColor ||
+          userConfig.darkVibrant !== serverUserConfig.darkVibrant ||
+          userConfig.lightVibrant !== serverUserConfig.lightVibrant
+        ) {
+          updateConfig.mutate({
+            userId: session.user.id,
+            autoThemeEnabled: userConfig.autoThemeEnabled ?? undefined,
+            tabs: userConfig.tabs ?? undefined,
+            themeImage: userConfig.themeImage ?? undefined,
+            searchEngine: userConfig.searchEngine ?? undefined,
+            topColor: userConfig.topColor ?? undefined,
+            bottomColor: userConfig.bottomColor ?? undefined,
+            darkVibrant: userConfig.darkVibrant ?? undefined,
+            lightVibrant: userConfig.lightVibrant ?? undefined,
+          });
+        }
+      } else {
+        createConfig.mutate({
+          autoThemeEnabled: userConfig.autoThemeEnabled ?? undefined,
+          tabs: userConfig.tabs ?? undefined,
+          themeImage: userConfig.themeImage ?? undefined,
+          searchEngine: userConfig.searchEngine ?? undefined,
+          topColor: userConfig.topColor ?? undefined,
+          bottomColor: userConfig.bottomColor ?? undefined,
+          darkVibrant: userConfig.darkVibrant ?? undefined,
+          lightVibrant: userConfig.lightVibrant ?? undefined,
+        });
+      }
+    }
   };
 
   return (
